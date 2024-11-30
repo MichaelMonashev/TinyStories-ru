6ccff8f710d7fc4a8b49f282b6f486527f274eb6
Michael Monashev <mmb@yandex.ru>
2024-11-30 14:53:09 +0300

    add Tokenizer() class

--- utils.py
+++ utils.py
@@ -1,5 +1,3 @@
-import torch
-
 class Vocab():
     def __init__(self, vocab:dict[str,int] = {}):
         self.vocab = vocab # str -> int
@@ -17,8 +15,28 @@ class Vocab():
     def __str__(self) -> str:
         return str(self.vocab)
 
+    def items(self) -> list[tuple[str, int]]:
+        return self.vocab.items()
+
     def make(self, text: str) -> int:
         chars = set(text)
         for char in chars:
             if char not in self.vocab:
                 self.vocab[char] = len(self.vocab)
+
+class Tokenizer():
+    def __init__(self, vocab):
+        self.vocab = vocab
+        self.inv_vocab = {v: k for k, v in self.vocab.items()}
+        return
+
+    # text -> tokens
+    def __call__(self, text: str) -> list[int]:
+        chars = list(text)
+        tokens = [self.vocab[char] for char in chars]
+        return tokens
+
+    # tokens -> text
+    def decode(self, tokens:list[int]) -> str:
+        text = [self.inv_vocab[t] for t in tokens]
+        return ''.join(text)
